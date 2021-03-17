1371dfd26f72d4623b8363cbc5f2705d8b972995
Charles Ivia <iviacharles@gmail.com>
2021-03-17 19:38:26 +0300

    Simulating probability of coin flip

---
+++ coin_flip.py
@@ -0,0 +1,52 @@
+# WANT: Simulate probability of head or tail such that, P(Head) = P(Tail) = 0.5
+
+# import libraries
+
+import random
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Coin flip function
+# 0 - Heads, 1 = Tails
+
+
+def coin_flip():
+    """return an integer from range of 0-1"""
+    return random.randint(0, 1)
+
+
+print(coin_flip())
+
+# Monte Carlo Simulation
+
+# Empty list to store the probability values
+list1 = []
+
+
+def monte_carlo(n):
+    results = 0
+    for i in range(n):
+        flip_result = coin_flip()
+        results = results + flip_result
+
+        # calculating the probability value
+        prob_value = results / (i + 1)
+        # Append prob values to list
+        list1.append(prob_value)
+
+        # plot the results
+
+        plt.axhline(y=0.5, color="r", linestyle="-")
+        plt.xlabel("Iterations")
+        plt.ylabel("Probability")
+        plt.plot(list1)
+
+    return results / n
+
+
+# calling the  function
+
+answer = monte_carlo(5000)
+print("Final value: ", answer)
+
+plt.show()
