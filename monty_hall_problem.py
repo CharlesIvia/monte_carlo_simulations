b21693389edeef65c4fcb80ac128dc357b66e2e9
Charles Ivia <iviacharles@gmail.com>
2021-03-18 16:59:26 +0300

    Monty Hall problem

---
+++ monty_hall_problem.py
@@ -0,0 +1,68 @@
+# WANT: Determine if a player has an advantage by switching doors in a game show.
+
+# READ: https://en.wikipedia.org/wiki/Monty_Hall_problem
+
+# Initial P(Car) = 1/3
+
+# P(Car) = 2/3 i.e. It increases after host opens one of the doors
+
+# Import libraries
+
+import random
+import matplotlib.pyplot as plt
+
+# We are going with 3 doors
+# 1 - Car
+# 2 - Goats
+doors = ["goat", "goat", "car"]
+
+# Empty lists to store probability values
+switch_win_probability = []
+stick_win_probability = []
+
+plt.axhline(y=0.66666, color="r", linestyle="-")
+plt.axhline(y=0.33333, color="g", linestyle="-")
+
+# Monte_Carlo Simulation
+
+
+def monte_carlo(n):
+    # Calculating switch and stick wins
+    switch_wins = 0
+    stick_wins = 0
+
+    for i in range(n):
+
+        # Randomly placing the car and goats behind the three doors
+        random.shuffle(doors)
+
+        # Contestant's choice
+        k = random.randrange(2)
+
+        # If the contestant doesn't get car
+        if doors[k] != "car":
+            switch_wins += 1
+
+        # If the contestant got car
+        else:
+            stick_wins += 1
+
+        # Updating the list values
+        switch_win_probability.append(switch_wins / (i + 1))
+        stick_win_probability.append(stick_wins / (i + 1))
+
+        # Plotting the data
+        plt.plot(switch_win_probability)
+        plt.plot(stick_win_probability)
+
+    # Print the probability values
+    print("Winning probability if you always switch:", switch_win_probability[-1])
+    print(
+        "Winning probability if you always stick to your original choice:",
+        stick_win_probability[-1],
+    )
+    plt.show()
+
+
+# Calling the function
+monte_carlo(2000)
